cfe08adf8355d3601339940c48d61b40316c50c9
Tobias Løfgren <tobloef@gmail.com>
2022-11-30 14:59:31 +0100

    Add PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
   base: "./",
   build: {
     outDir: "./docs"
