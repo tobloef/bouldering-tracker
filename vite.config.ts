d9e6e53df9ce0c37a11d4cf7c0e23621f6b23680
Tobias Løfgren <tobloef@gmail.com>
2022-11-30 14:59:31 +0100

    Add PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'masked-icon.svg',
+        'mask-icon-512.png',
+        'mask-icon-192.png',
+      ],
+      manifest: {
+        name: 'Bouldering Tracker',
+        short_name: 'BoulderTrack',
+        description: 'My Personal Bouldering Tracker',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'mask-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'mask-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    }),
+  ],
   base: "./",
   build: {
     outDir: "./docs"
